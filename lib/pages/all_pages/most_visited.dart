0044c047913433cc9f3ead41e021e53d19611646
Shiva Shrestha <shivashrestha1056@gmail.com>
2025-01-28 01:14:32 +0545

    loading horizontally updated

--- lib/pages/all_pages/most_visited.dart
+++ lib/pages/all_pages/most_visited.dart
@@ -9,7 +9,8 @@ import 'package:intl/intl.dart';
 import 'package:shimmer/shimmer.dart';
 
 class MostVisited extends StatefulWidget {
-  const MostVisited({Key? key}) : super(key: key);
+  bool? mostvisited;
+  MostVisited({Key? key, this.mostvisited}) : super(key: key);
 
   @override
   State<MostVisited> createState() => _MostVisitedState();
@@ -55,8 +56,8 @@ class _MostVisitedState extends State<MostVisited> {
 
   @override
   Widget build(BuildContext context) {
-     double height=MediaQuery.of(context).size.height;
-    double width=MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     // if (isLoading) {
     //   return Center(child: CircularProgressIndicator());
     // }
@@ -67,19 +68,52 @@ class _MostVisitedState extends State<MostVisited> {
     return Container(
       color: Colors.white,
       padding: EdgeInsets.only(left: 6, bottom: 30),
-      height: height*0.36,
-      child: GridView.builder(
-        scrollDirection: Axis.horizontal,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          childAspectRatio: 1.6,
-          crossAxisCount: 1,
-          // childAspectRatio: 1.5, // Adjust aspect ratio for better spacing
-        ),
-        itemCount: categoriesList.length,
-        itemBuilder: (context, index) {
-          return CategoryCard(category: categoriesList[index]);
-        },
-      ),
+      height: height * 0.36,
+      child: (isLoading || widget.mostvisited!)
+          ? GridView.builder(
+              scrollDirection: Axis.horizontal,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                childAspectRatio: 1.6,
+                crossAxisCount: 1,
+                // childAspectRatio: 1.5, // Adjust aspect ratio for better spacing
+              ),
+              itemCount: categoriesList.length,
+              itemBuilder: (context, index) {
+                return Shimmer.fromColors(
+                  baseColor: Colors.grey.shade300, // Darker base color
+                  highlightColor: Colors.white, // Lighter highlight color
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 2, vertical: 2),
+                    margin:
+                        EdgeInsets.only(left: 10, right: 10, top: 2, bottom: 2),
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.4),
+                          spreadRadius: 3,
+                          blurRadius: 3,
+                          offset: Offset(2, 2),
+                        ),
+                      ],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                );
+              },
+            )
+          : GridView.builder(
+              scrollDirection: Axis.horizontal,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                childAspectRatio: 1.6,
+                crossAxisCount: 1,
+                // childAspectRatio: 1.5, // Adjust aspect ratio for better spacing
+              ),
+              itemCount: categoriesList.length,
+              itemBuilder: (context, index) {
+                return CategoryCard(category: categoriesList[index]);
+              },
+            ),
     );
   }
 }
@@ -91,20 +125,20 @@ class CategoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double height=MediaQuery.of(context).size.height;
-    double width=MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Container(
       margin: EdgeInsets.only(left: 2),
       padding: EdgeInsets.all(3),
       decoration: BoxDecoration(
         boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              spreadRadius: 1,
-              blurRadius: 1,
-              offset: Offset(2, 2),
-            ),
-          ],
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            spreadRadius: 1,
+            blurRadius: 1,
+            offset: Offset(2, 2),
+          ),
+        ],
       ),
       child: CupertinoButton(
         color: Colors.white,
@@ -120,9 +154,9 @@ class CategoryCard extends StatelessWidget {
           );
         },
         child: Container(
-          width: width*0.4,
-          height: height*0.31,
-          margin: EdgeInsets.only(right: 2,left: 2,top: 2), // Reduced margin
+          width: width * 0.4,
+          height: height * 0.31,
+          margin: EdgeInsets.only(right: 2, left: 2, top: 2), // Reduced margin
           decoration: BoxDecoration(
             // boxShadow: [
             //   BoxShadow(
@@ -144,59 +178,82 @@ class CategoryCard extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       Container(
-                       
-
-                       
-                      
-                        
                         height: 160,
                         width: 160,
                         child: ClipRRect(
                           borderRadius: BorderRadius.circular(8),
                           child: CachedNetworkImage(
-                              imageUrl: category.image[0],
-                              height: 160,
-                              width: 160,
-                              fit: BoxFit.cover,
-                              placeholder: (context, url) => Shimmer.fromColors(
-                                        baseColor: Colors.grey.shade300, // Darker base color
-                                        highlightColor: Colors.white,    // Lighter highlight color
-                                        child: Container(
-                                         
-                                          padding: EdgeInsets.symmetric(horizontal: 2, vertical: 2),
-                                          margin: EdgeInsets.only(left: 10, right: 10, top: 2, bottom: 2),
-                                          decoration: BoxDecoration(
-                                            boxShadow: [
-                                              BoxShadow(
-                                                color: Colors.black.withOpacity(0.4),
-                                                spreadRadius: 3,
-                                                blurRadius: 3,
-                                                offset: Offset(2, 2),
-                                              ),
-                                            ],
-                                            color: Colors.white,
-                                            borderRadius: BorderRadius.circular(8),
-                                          ),
-                                        ),
-                                      
-                              
-                              
+                            imageUrl: category.image[0],
+                            height: 160,
+                            width: 160,
+                            fit: BoxFit.cover,
+                            placeholder: (context, url) =>
+
+                                // Shimmer.fromColors(
+                                //   baseColor:
+                                //       Colors.grey.shade300, // Darker base color
+                                //   highlightColor:
+                                //       Colors.white, // Lighter highlight color
+                                //   child: Container(
+                                //     padding: EdgeInsets.symmetric(
+                                //         horizontal: 2, vertical: 2),
+                                //     margin: EdgeInsets.only(
+                                //         left: 10, right: 10, top: 2, bottom: 2),
+                                //     decoration: BoxDecoration(
+                                //       boxShadow: [
+                                //         BoxShadow(
+                                //           color: Colors.black.withOpacity(0.4),
+                                //           spreadRadius: 3,
+                                //           blurRadius: 3,
+                                //           offset: Offset(2, 2),
+                                //         ),
+                                //       ],
+                                //       color: Colors.white,
+                                //       borderRadius: BorderRadius.circular(8),
+                                //     ),
+                                //   ),
+                                // ),
+                                GridView.builder(
+                              scrollDirection: Axis.horizontal,
+                              gridDelegate:
+                                  SliverGridDelegateWithFixedCrossAxisCount(
+                                childAspectRatio: 1.6,
+                                crossAxisCount: 1,
+                                // childAspectRatio: 1.5, // Adjust aspect ratio for better spacing
                               ),
-                              errorWidget: (context, error, stackTrace) => Center(child: Icon(Icons.error)),
+                              itemCount: 2,
+                              itemBuilder: (context, index) {
+                                return Shimmer.fromColors(
+                                  baseColor:
+                                      Colors.grey.shade300, // Darker base color
+                                  highlightColor:
+                                      Colors.white, // Lighter highlight color
+                                  child: Container(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 2, vertical: 2),
+                                    margin: EdgeInsets.only(
+                                        left: 10, right: 10, top: 2, bottom: 2),
+                                    decoration: BoxDecoration(
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.black.withOpacity(0.4),
+                                          spreadRadius: 3,
+                                          blurRadius: 3,
+                                          offset: Offset(2, 2),
+                                        ),
+                                      ],
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                  ),
+                                );
+                              },
                             ),
+                            errorWidget: (context, error, stackTrace) =>
+                                Center(child: Icon(Icons.error)),
+                          ),
                         ),
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
+
                         //  ClipRRect(
                         //   borderRadius: BorderRadius.circular(8),
                         //   child: Image.network(
@@ -216,7 +273,7 @@ class CategoryCard extends StatelessWidget {
                               height: 20,
                             ),
                             //  Text(calculateTimeDifference(category.date), style: TextStyle(fontSize: 14)),
-      
+
                             Padding(
                               padding: const EdgeInsets.only(left: 2),
                               child: Text("Rs. ${category.rent}",
@@ -228,11 +285,19 @@ class CategoryCard extends StatelessWidget {
                             ),
                             Padding(
                               padding: const EdgeInsets.only(left: 2),
-                              child: Text(category.address,
-                                  style: TextStyle(fontWeight: FontWeight.bold,
-                                      fontSize: 16, color: Colors.black)),
+                              child: Text(
+  category.address.isNotEmpty
+      ? category.address[0].toUpperCase() + category.address.substring(1)
+      : "",  // In case the address is empty, it avoids showing null or empty text
+  style: TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 16,
+    color: Colors.black,
+  ),
+),
+
                             ),
-      
+
                             SizedBox(
                               height: 5,
                             ),
@@ -269,10 +334,9 @@ class CategoryCard extends StatelessWidget {
                 right: 10,
                 child: Container(
                   padding: EdgeInsets.all(6),
-                  decoration: BoxDecoration(  
-                    borderRadius: BorderRadius.all(Radius.circular(100)),
-                    color: Colors.grey.shade300
-                  ),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(100)),
+                      color: Colors.grey.shade300),
                   child: Icon(
                     category.isFavourite ? Icons.lock_open : Icons.lock_outline,
                     color: category.isFavourite ? Colors.green : Colors.red,
